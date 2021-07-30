f6dff436d4223c9ed550ca22f9f4f5160f3db7cb
jzj <jzj1993@gmail.com>
2021-07-31 05:26:53 +0800

    TestNFT

--- contracts/TestNFT.sol
+++ contracts/TestNFT.sol
@@ -29,9 +29,9 @@ contract TestNFT is ERC721 {
                     bytes(
                         abi.encodePacked(
                             '{"name":"',
-                            uint2str(tokenId),
+                            'Token #', Strings.toString(tokenId),
                             '", "description":"',
-                            "my desc",
+                            "Description Description Description Description Description Description Description Description Description Description",
                             '", "image": "',
                             'data:image/svg+xml;base64,',
                             image,
@@ -41,29 +41,6 @@ contract TestNFT is ERC721 {
                 )
             )
         );
-    }
-
-    //https://stackoverflow.com/questions/47129173/how-to-convert-uint-to-string-in-solidity
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len;
-        while (_i != 0) {
-            k = k-1;
-            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
     }
 
     // return github logo svg
